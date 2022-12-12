626d4354ad984d4f257ed479cc759231c53e0d89
mAbduqayum <mabduqayum@gmail.com>
2022-12-12 22:34:41 +0500

    updated routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import {RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
   {path: '', loadChildren: () => import('./main/main.module').then(m => m.MainModule)},
   {path: 'main', redirectTo: ''},
-  { path: 'shapes', loadChildren: () => import('./shapes/shapes.module').then(m => m.ShapesModule) },
-  { path: 'compare', loadChildren: () => import('./compare/compare.module').then(m => m.CompareModule) },
-  { path: 'coins', loadChildren: () => import('./coins/coins.module').then(m => m.CoinsModule) }
+  {path: 'shapes', loadChildren: () => import('./shapes/shapes.module').then(m => m.ShapesModule)},
+  {path: 'compare', loadChildren: () => import('./compare/compare.module').then(m => m.CompareModule)},
+  {path: 'coins', loadChildren: () => import('./coins/coins.module').then(m => m.CoinsModule)},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
